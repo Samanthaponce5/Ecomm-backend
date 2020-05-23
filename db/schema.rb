46991eadffde25ec76bb8ce1a6dec5cc70e56e57
Samanthaponce5 <samanthaponce5@gmail.com>
2020-05-22 21:48:27 -0400

    part1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_022940) do
+ActiveRecord::Schema.define(version: 2020_05_22_233536) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_05_22_022940) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-ActiveRecord::Schema.define(version: 2020_05_22_233536) do
 
   create_table "orders", force: :cascade do |t|
     t.string "confirmation_number"
