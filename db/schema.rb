ca70d4d0ac4460c1c609a1200dde74838b8ac6c0
JohnsonNYC <jkow95@gmail.com>
2020-05-25 12:21:28 -0400

    changed price from decimal to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_24_232839) do
+ActiveRecord::Schema.define(version: 2020_05_25_161828) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2020_05_24_232839) do
     t.string "title"
     t.string "type_of"
     t.string "category"
-    t.decimal "price"
+    t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
